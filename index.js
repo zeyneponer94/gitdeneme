40c75828846cfe18d3e5fdb411b52cd1451788a6
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-15 23:00:01 +0300

    Demo

--- index.js
+++ index.js
@@ -2,32 +2,34 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var dbOperations = require("./dbOperations.js");
+
+
 var app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .all('/userdata', (req, res) =>  res.sendStatus(200))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+      /*
+       var email = req.body.email;
+       var pass = req.body.password;
+       var data = {
+       "Data":""
+       };
+       connection.query("SELECT * from login WHERE email=? and password=? LIMIT 1",[email,pass],function(err, rows, fields){
+       if(rows.length != 0){
+       data["Data"] = "Successfully logged in..";
+       res.json(data);
+       }else{
+       data["Data"] = "Email or password is incorrect.";
+       res.json(data);
+       }
+       });*/
 
-var dbOperations = require("./dbOperations.js");
 
-app.post('/userdata',function(req,res){
-         
-         res.sendStatus(200);
-         /*
-         var email = req.body.email;
-         var pass = req.body.password;
-         var data = {
-         "Data":""
-         };
-         connection.query("SELECT * from login WHERE email=? and password=? LIMIT 1",[email,pass],function(err, rows, fields){
-                          if(rows.length != 0){
-                          data["Data"] = "Successfully logged in..";
-                          res.json(data);
-                          }else{
-                          data["Data"] = "Email or password is incorrect.";
-                          res.json(data);
-                          }
-                          });*/
-         });
+
+
+
