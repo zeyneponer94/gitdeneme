1d18ce375e38e1ab9d60cf91907c66abd10eea82
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-15 22:55:12 +0300

    Demo

---
+++ dbOperations.js
@@ -0,0 +1,26 @@
+module.exports = {
+    
+    
+    
+    userdata : function(req, res){
+  /*      var pg = require('pg');
+        
+        var conString = process.env.postgres://meivxnuxakxqvi:e501dbd3feafa616c3d31c68cc2c51474064ac1aefdcf395838fb79da3812cca@ec2-54-83-203-198.compute-1.amazonaws.com:5432/d9q6ascc3tnn62;
+        var client = new pg.Client(conString);
+        
+        client.connect();
+        
+        
+        var query = client.query("insert into user_DATA (username__c, password__c) "+
+                                 "values ('"+req.query.username_id+"','"+req.query.password_id+"')");
+        
+        query.on("end", function (result) {
+                 client.end();
+                 res.write('Success');
+                 res.end();
+                 });*/
+        
+    }
+ 
+    
+};
