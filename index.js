0338ca7417db372a9863bae99cab9d8385b3b491
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-17 00:35:05 +0300

    Demo

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-var express = require('express'),
-    http = require('http'),
-    request = require('request'),
-    bodyParser = require('body-parser'),
-    errorHandler = require('express-error-handler'),
-    app = express();
-
-var dbOperations = require("./dbOperations.js");
-var logFmt = require("logfmt");
-
-app.set('views', path.join(__dirname, 'views'));
-
-app.get('/' , function(req,res) {
-    res.sendStatus(200);
-});
-
-app.post('/userdata' , function(req,res) {
-    res.sendStatus(200);
-});
-
-
-
-app.set('port', process.env.PORT || 5000);
-app.use(express.static(path.join(__dirname, 'public')));
-app.listen(app.get('port'), function () {
-});
-
 
 /*
 
@@ -38,3 +11,16 @@ app.all('/userdata' , function(req,res) {
 
 
 */
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+var dbOperations = require("./dbOperations.js");
+
+express()
+.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
+.get('/', (req, res) => res.render('pages/index'))
+.post('/userdata', (req, res) => res.sendStatus(200))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
