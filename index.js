0e9251dbf302e71ad0b7124c3b994439169a5363
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-16 23:05:41 +0300

    Demo

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var app = express()
 router.post('/userdata', (req, res, next) => {
             const results = [];
             // Grab data from http request
-            const data = {text: req.body.text, complete: false};
+            const data = {text: req.body};
             // Get a Postgres client from the connection pool
             pg.connect(conString, (err, client, done) => {
                        // Handle connection errors
@@ -30,7 +30,7 @@ router.post('/userdata', (req, res, next) => {
                        }
                        
                        client.query('INSERT INTO user_data(user_id, username, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, type, timestamp) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
-                                    [data.text.user_id, data.text.username, data.text.acc_x, data.text.acc_y, data.text.acc_z, data.text.gyro_x, data.text.gyro_y, data.text.gyro_z, data.tex.type, data.text.timestamp]);
+                                    [data.text.user_id, data.text.username, data.text.acc_x, data.text.acc_y, data.text.acc_z, data.text.gyro_x, data.text.gyro_y, data.text.gyro_z, data.text.type, data.text.timestamp]);
                        // SQL Query > Select Data
                        const query = client.query('SELECT * FROM user_data');
                        // Stream results back one row at a time
