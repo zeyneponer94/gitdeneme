75ce01b10a3b1eb24d3edd87b40cf4de185910c3
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-17 00:40:48 +0300

    Demo

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var dbOperations = require("./dbOperations.js");
 
 express()
 .use(express.static(path.join(__dirname, 'public')))
-.set('views', path.join(__dirname, 'views'))
+.set('views', path.join(__dirname, '/app/views'))
 .set('view engine', 'ejs')
 .get('/', (req, res) => res.render('pages/index'))
 .post('/userdata', (req, res) => res.sendStatus(200))
