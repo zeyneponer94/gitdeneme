45e2a16ceef067f406739478f95002160527163d
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-16 23:26:00 +0300

    Demo

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var dbOperations = require("./dbOperations.js");
-var pg = require('pg');
-const router = express.Router();
-var conString = "postgres://meivxnuxakxqvi:e501dbd3feafa616c3d31c68cc2c51474064ac1aefdcf395838fb79da3812cca@ec2-54-83-203-198.compute-1.amazonaws.com:5432/d9q6ascc3tnn62";
+
 
 
 var app = express()
@@ -16,41 +14,11 @@ var app = express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
-router.post('/userdata', (req, res, next) => {
-            const results = [];
-            // Grab data from http request
-            const data = {text: req.body};
-            var obj = JSON.parse(data);
-            // Get a Postgres client from the connection pool
-            pg.connect(conString, (err, client, done) => {
-                       // Handle connection errors
-                       if(err) {
-                       done();
-                       console.log(err);
-                       return res.status(500).json({success: false, data: err});
-                       }
-                       
-                       client.query('INSERT INTO user_data(user_id, username, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, type, timestamp) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
-                                    [obj.text.user_id, obj.text.username, obj.text.acc_x, obj.text.acc_y, obj.text.acc_z, obj.text.gyro_x, obj.text.gyro_y, obj.text.gyro_z, obj.text.type, obj.text.timestamp]);
-                       // SQL Query > Select Data
-                       const query = client.query('SELECT * FROM user_data');
-                       // Stream results back one row at a time
-                       query.on('row', (row) => {
-                                results.push(row);
-                                });
-                       // After all data is returned, close connection and return results
-                       query.on('end', () => {
-                                done();
-                                return res.json(results);
-                                });
-                       });
-            });
 
-/*
 app.all('/userdata' , function(req,res) {
         dbOperations.userdata(req,res);
         res.sendStatus(200);
-});*/
+});
 
 
 
