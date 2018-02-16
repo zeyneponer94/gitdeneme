9914ca08ab7ebace3a2e88ef93cbf31c2883c27d
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-16 23:59:43 +0300

    Demo

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-var express = require('express')
-var path = require('path')
-const PORT = process.env.PORT || 5000
-
-var http = require('http'),
-request = require('request'),
-bodyParser = require('body-parser'),
-errorHandler = require('express-error-handler');
-
+var express = require('express'),
+    http = require('http'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    errorHandler = require('express-error-handler'),
+    app = express();
 
 var dbOperations = require("./dbOperations.js");
 var logFmt = require("logfmt");
 
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/' , function(req,res) {
+    res.sendfile('pages/index.html');
+});
+
+app.post('/userdata' , function(req,res) {
+    res.sendStatus(200);
+});
 
 
-var app = express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .all('/userdata', (req, res) => res.sendStatus(200))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.set('port', process.env.PORT || 5000);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(errorHandler());
+app.listen(app.get('port'), function () {
+});
+
 
 /*
 
