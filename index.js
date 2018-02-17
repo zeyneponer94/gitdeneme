9bd0c4f15ceb29566dd7bb20b6fdd63029ab8888
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-17 12:33:40 +0300

    Demo

--- index.js
+++ index.js
@@ -13,15 +13,17 @@ app.all('/userdata' , function(req,res) {
 */
 
 const express = require('express')
+
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var dbOperations = require("./dbOperations.js");
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
+    .use(express.bodyParser())
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .all('/userdata', (req, res) => res.send(req.data.user_id))
+    .all('/userdata', (req, res) => res.send(req.body))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 //dbOperations.userdata(req,res)
