52675ed4560ae371ec7b27fd859accb1277e0579
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-17 13:56:54 +0300

    Demo

--- dbOperations.js
+++ dbOperations.js
@@ -8,8 +8,8 @@ module.exports = {
 
         var pg = require('pg');
         
-        //const conString = 'postgres://meivxnuxakxqvi:e501dbd3feafa616c3d31c68cc2c51474064ac1aefdcf395838fb79da3812cca@ec2-54-83-203-198.compute-1.amazonaws.com:5432/d9q6ascc3tnn62';
-        const conString = 'postgres://onerz15@itu.edu.tr:itu_cse_2018@localhost:5000/postgresql-defined-42923'
+        const conString = 'postgres://meivxnuxakxqvi:e501dbd3feafa616c3d31c68cc2c51474064ac1aefdcf395838fb79da3812cca@ec2-54-83-203-198.compute-1.amazonaws.com:5432/d9q6ascc3tnn62';
+       // const conString = 'postgres://onerz15@itu.edu.tr:itu_cse_2018@localhost:5000/postgresql-defined-42923'
         
         var client = new pg.Client(conString);
         client.connect();
