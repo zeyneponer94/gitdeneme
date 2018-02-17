012979648d97aaf2fc94913293aacba48b22b201
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-17 13:23:21 +0300

    Demo

--- dbOperations.js
+++ dbOperations.js
@@ -9,7 +9,7 @@ module.exports = {
         var pg = require('pg');
         
         //const conString = 'postgres://meivxnuxakxqvi:e501dbd3feafa616c3d31c68cc2c51474064ac1aefdcf395838fb79da3812cca@ec2-54-83-203-198.compute-1.amazonaws.com:5432/d9q6ascc3tnn62';
-        const conString = 'postgres://onerz15@itu.edu.tr:itu_cse_2018@localhost/user_data_'
+        const conString = 'postgres://onerz15@itu.edu.tr:itu_cse_2018@localhost/postgresql-defined-42923'
         const data = req.body;
         pg.connect(conString, function(err,client,done){
                    if(err) return next(err)
