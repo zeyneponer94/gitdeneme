7b7eb549449f22c7f3152eaf9d7c5ce87173fd56
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-17 13:17:34 +0300

    Demo

--- dbOperations.js
+++ dbOperations.js
@@ -8,17 +8,31 @@ module.exports = {
 
         var pg = require('pg');
         
-        var conString = "postgres://meivxnuxakxqvi:e501dbd3feafa616c3d31c68cc2c51474064ac1aefdcf395838fb79da3812cca@ec2-54-83-203-198.compute-1.amazonaws.com:5432/d9q6ascc3tnn62";
+        const conString = 'postgres://meivxnuxakxqvi:e501dbd3feafa616c3d31c68cc2c51474064ac1aefdcf395838fb79da3812cca@ec2-54-83-203-198.compute-1.amazonaws.com:5432/d9q6ascc3tnn62';
+        const data = req.body;
+        pg.connect(conString, function(err,client,done){
+                   if(err) return next(err)
+                   client.query('INSERT INTO user_data_(user_id, username, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, type, timestamp) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);',[data.user_id, data.username, data.acc_x, data.acc_y, data.acc_z, data.gyro_x, data.gyro_y, data.gyro_z, data.type, data.timestamp],function(err,result){
+                                done()
+                                if(err) return next(err)
+                                res.send(200)
+                                
+                        })
+                   
+                   
+        })
+        
+        /*
         var client = new pg.Client(conString);
         client.connect();
         // Grab data from http request
         const data = req.body;
         var results = [];
         
-        var query = client.query("INSERT INTO user_data_(user_id, username, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, type, timestamp) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [data.user_id, data.username, data.acc_x, data.acc_y, data.acc_z, data.gyro_x, data.gyro_y, data.gyro_z, data.type, data.timestamp]);
+        client.query("INSERT INTO user_data_(user_id, username, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, type, timestamp) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [data.user_id, data.username, data.acc_x, data.acc_y, data.acc_z, data.gyro_x, data.gyro_y, data.gyro_z, data.type, data.timestamp]);
         
         // SQL Query > Select Data
-         query = client.query("SELECT * FROM user_data");
+        var query = client.query("SELECT * FROM user_data");
         // Stream results back one row at a time
         var count = 0;
         query.rows.forEach(row=>{
@@ -32,7 +46,7 @@ module.exports = {
             res.write(JSON.stringify(results, null, "    ") + "\n");
         else
             res.write('0');
-        res.end();
+        res.end();*/
         /*
                    query.on('row', (row) => {
                             results.push(row);
