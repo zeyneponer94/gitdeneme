882dbddc976c27c1976a70b9ab3e8029b95a1e1b
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-17 13:41:53 +0300

    Demo

--- dbOperations.js
+++ dbOperations.js
@@ -17,10 +17,10 @@ module.exports = {
         const data = req.body;
         var results = [];
         
-      //  client.query('INSERT INTO user_data_(user_id, username, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, type, timestamp) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [data.user_id, data.username, data.acc_x, data.acc_y, data.acc_z, data.gyro_x, data.gyro_y, data.gyro_z, data.type, data.timestamp]);
+        client.query('INSERT INTO user_data_(user_id, username, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, type, timestamp) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [data.user_id, data.username, data.acc_x, data.acc_y, data.acc_z, data.gyro_x, data.gyro_y, data.gyro_z, data.type, data.timestamp]);
         
         // SQL Query > Select Data
-        var query = client.query('SELECT COUNT(*) FROM user_data_');
+        var query = client.query('SELECT * FROM user_data_');
         // Stream results back one row at a time
         //var count = 0;
       /*  query.rows.forEach(row=>{
@@ -28,13 +28,14 @@ module.exports = {
              count++;
         });*/
         client.end();
-        res.send(""+query);
+        res.send(query.rows);
+        
+        
         res.writeHead(200, {'Content-Type': 'text/plain'});
         if (query!=0)
             res.write(JSON.stringify(results, null, "    ") + "\n");
         else
             res.write('0');
-        res.send(""+query);
         res.end();
         /*
                    query.on('row', (row) => {
