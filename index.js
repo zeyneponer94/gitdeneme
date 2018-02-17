e133de5224cebf4febd423de153e849df6006342
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-17 12:20:36 +0300

    Demo

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ const PORT = process.env.PORT || 5000
 var dbOperations = require("./dbOperations.js");
 
 express()
+    .all('*', function(req, res, next) {
+        var origin = req.get('origin');
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    });
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
