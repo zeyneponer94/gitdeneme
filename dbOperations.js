dfc749f78c9ae493a2090e44f2b05a3d50e94790
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-17 12:53:18 +0300

    Demo

--- dbOperations.js
+++ dbOperations.js
@@ -4,9 +4,8 @@ module.exports = {
     
     userdata : function(req, res){
         
-        res.send(req.body);
 
-/*
+
         var pg = require('pg');
         
         var conString = "postgres://meivxnuxakxqvi:e501dbd3feafa616c3d31c68cc2c51474064ac1aefdcf395838fb79da3812cca@ec2-54-83-203-198.compute-1.amazonaws.com:5432/d9q6ascc3tnn62";
@@ -14,10 +13,8 @@ module.exports = {
         client.connect();
         // Grab data from http request
         const data = req.body;
-        var obj = JSON.parse(req.body);
         
-        res.send(req.body);*/
-        /*
+        
         var query = client.query("INSERT INTO user_data_(user_id, username, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, type, timestamp) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [obj.user_id, obj.username, obj.acc_x, obj.acc_y, obj.acc_z, obj.gyro_x, obj.gyro_y, obj.gyro_z, obj.type, obj.timestamp]);
         
         // SQL Query > Select Data
@@ -39,7 +36,7 @@ module.exports = {
             else
                  res.write('0');
             res.end();
-        });*/
+        });
         /*
                    query.on('row', (row) => {
                             results.push(row);
