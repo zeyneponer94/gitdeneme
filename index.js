43c2799d18ae55167b759d2bdecaa108b3532a16
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-17 11:57:56 +0300

    Demo

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const PORT = process.env.PORT || 5000
 var dbOperations = require("./dbOperations.js");
 
 express()
-.use(express.static(path.join(__dirname, 'public')))
-.set('views', path.join(__dirname, '/gitdeneme/views'))
-.set('view engine', 'ejs')
-.get('/', (req, res) => res.render('pages/index'))
-.post('/userdata', (req, res) => res.sendStatus(200))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .all('/userdata', (req, res) => res.sendStatus(200))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
